25eebd8872f14d805faf7add653ca0961b04112c
Rafael Mohlin <raffe_mohlin@hotmail.com>
2018-12-02 15:39:17 +0100

    Day 02 task 02

---
+++ 02/task02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+from collections import defaultdict
+
+combos = defaultdict(set)
+for line in sys.stdin.read().splitlines():
+    for i in range(len(line)):
+        trunc = line[:i] + line[i+1:]
+        if trunc in combos[i]:
+            print(trunc)
+            exit()
+        combos[i].add(trunc)
