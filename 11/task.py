6b6c14c89938d0560b3186d7039cf3637dff36dc
Rafael Mohlin <raffe_mohlin@hotmail.com>
2018-12-15 23:57:35 +0100

    Day 11: Summed-area table

--- 11/task.py
+++ 11/task.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+from collections import defaultdict
 
 if len(sys.argv) < 2 or len(sys.argv) > 3:
     print("Usage: {} INPUT SIZE".format(sys.argv[0]))
@@ -23,30 +24,21 @@ def power_level(grid_sn, cell_pos):
     rack_id = x + 10
     return (((((rack_id * y) + grid_sn) * rack_id) // 100) % 10) - 5
 
-grid = dict()
+grid = defaultdict(int)
 for y in range(1, grid_size + 1):
     for x in range(1, grid_size + 1):
         grid[(x, y)] = power_level(grid_sn, (x, y))
 
-def all_windows(grid, grid_size, window_size):
-    if len(sys.argv) < 3:
-        print("{:.2f}%".format(100 * window_size / grid_size))
-    last_lvl = 0
-    for y in range(1, 1 + window_size - 1):
-        for x in range(1, 1 + window_size - 1):
-            last_lvl += grid[(x, y)]
-    for window_y in range(1, grid_size + 1 - window_size + 1):
-        for x in range(1, 1 + window_size - 1):
-            last_lvl += grid[(x, window_y + window_size -1)]
-        lvl = last_lvl
-        for x in range(1, 1 + window_size - 1):
-            last_lvl -= grid[(x, window_y)]
-        for window_x in range(1, grid_size + 1 - window_size + 1):
-            for y in range(window_y, window_y + window_size):
-                lvl += grid[(window_x + window_size - 1, y)]
-            yield (window_x, window_y, window_size, lvl)
-            for y in range(window_y, window_y + window_size):
-                lvl -= grid[(window_x, y)]
+summed = defaultdict(int)
+for y in range(1, grid_size + 1):
+    for x in range(1, grid_size + 1):
+        summed[(x, y)] = grid[(x, y)] + summed[(x, y - 1)] + summed[(x - 1, y)] - summed[(x - 1, y - 1)]
 
-window_x, window_y, window_size, lvl = max(map(lambda window_size: max(all_windows(grid, grid_size, window_size), key=lambda w: w[3]), window_sizes), key=lambda candidate: candidate[3])
+def all_windows(summed, grid_size, window_size):
+    for y in range(1, grid_size + 1 - window_size + 1):
+        for x in range(1, grid_size + 1 - window_size + 1):
+            lvl = summed[(x + window_size - 1, y + window_size - 1)] + summed[(x - 1, y - 1)] - summed[(x + window_size - 1, y - 1)] - summed[(x - 1, y + window_size - 1)]
+            yield (x, y, window_size, lvl)
+
+window_x, window_y, window_size, lvl = max(map(lambda window_size: max(all_windows(summed, grid_size, window_size), key=lambda w: w[3]), window_sizes), key=lambda w: w[3])
 print("largest window of size {}x{} found @ ({}, {}): {}".format(window_size, window_size, window_x, window_y, lvl))
